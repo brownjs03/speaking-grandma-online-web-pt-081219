3c73d02102a228e3707dffef76336bad80696dac
Jeffrey Brown <brownjs03@gmail.com>
2019-08-15 21:35:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 def speak_to_grandma(val)
   if val == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
-elsif val 
+elsif val == val.upcase
+  "NO, NOT SINCE 1938!"
 else
   "HUH?! SPEAK UP, SONNY!"
 end
